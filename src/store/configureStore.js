385bdf25f7ac8405d3313cf4625334f809293898
Aaron McClinchey <8866130+disciple-dev@users.noreply.github.com>
2022-12-10 23:49:01 -0700

    don't use logger in production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-const middleware = [logger];
+const dev = process.env.NODE_ENV === "development";
+
+const middleware = [];
+
+if (dev) middleware.push(logger);
 
 export default function configureAppStore(preloadedState) {
   const store = configureStore({
