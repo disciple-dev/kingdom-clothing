573ab2fad1cab60a2af72d8d8b0e477b4b593277
Aaron McClinchey <8866130+disciple-dev@users.noreply.github.com>
2023-01-05 09:58:42 -0700

    add PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import App from "./App";
 import { PersistGate } from "redux-persist/integration/react";
 import { Elements } from "@stripe/react-stripe-js";
 import { stripePromise } from "./services/stripe/stripe.utils";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import { store, persistor } from "./store/configureStore";
 import reportWebVitals from "./reportWebVitals";
@@ -23,6 +24,8 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
